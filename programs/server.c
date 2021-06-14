e3f91d84a85d73b50c199bdbe94624b8d07ba703
bencamos <85905323+bencamos@users.noreply.github.com>
2021-06-15 06:57:25 +1000

    Add files via upload

---
+++ programs/server.c
@@ -0,0 +1,63 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 48567
+
+char* concat(const char *s1, const char *s2)
+{
+    const size_t len1 = strlen(s1);
+    const size_t len2 = strlen(s2);
+    char *result = malloc(len1 + len2 + 1);
+    memcpy(result, s1, len1);
+    memcpy(result + len1, s2, len2 + 1);
+    return result;
+}
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0){
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0){
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0){
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read( new_socket , buffer, 1024);
+    FILE *fp,*outputfile;
+    char var[1024];
+    char* first = "exec bash -c \"";
+    char* second = "\"";
+    char* third = concat(first, buffer);
+    char* full = concat(third, second);
+    fp = popen(full, "r");
+    while (fgets(var, sizeof(var), fp) != NULL){
+        printf("%s", var);
+        send(new_socket , var , strlen(var) , 0 );
+    }
+    pclose(fp);
+    return 0;
+}
